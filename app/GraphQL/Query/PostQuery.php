90640e4c7f713258e8e83fe21143713295e1cd86
Maciej Sobaczewski <msobaczewski@gmail.com>
2018-06-11 12:20:28 +0200

    Solve n+1 problem

--- app/GraphQL/Query/PostQuery.php
+++ app/GraphQL/Query/PostQuery.php
@@ -3,9 +3,11 @@
 namespace App\GraphQL\Query;
 
 use App\Model\Post;
+use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Facades\GraphQL;
 use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\SelectFields;
 
 class PostQuery extends Query
 {
@@ -28,12 +30,14 @@ class PostQuery extends Query
         ];
     }
 
-    public function resolve($root, $args)
+    public function resolve($root, $args, SelectFields $fields)
     {
+        $with = $fields->getRelations();
+
         if (isset($args['id'])) {
-            return Post::where('id', $args['id'])->get();
+            return Post::with($with)->where('id', $args['id'])->get();
         } else {
-            return Post::all();
+            return Post::with($with)->get();
         }
     }
 }
